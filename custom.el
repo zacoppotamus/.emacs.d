6fdd637ab0278191eed5274457145c69ee7d7297
Zac Ioannidis <zacoppotamus@gmail.com>
2020-07-08 14:37:25 +0300

    Added org-roam and org-journal

--- custom.el
+++ custom.el
@@ -8,12 +8,17 @@
  '(custom-safe-themes
    (quote
     ("732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "2d835b43e2614762893dc40cbf220482d617d3d4e2c35f7100ca697f1a388a0e" default)))
+ '(global-vi-tilde-fringe-mode nil)
  '(line-number-mode nil)
  '(neo-autorefresh t)
  '(neo-theme (quote icons))
+ '(org-agenda-files
+   (quote
+    ("/Users/zac/org/projects.org" "/Users/zac/org/code.org" "/Users/zac/org/emacs.org" "/Users/zac/org/index.org")))
+ '(org-journal-dir "~/Documents/org/journal")
  '(package-selected-packages
    (quote
-    (eyebrowse vterm cider paredit clojure-mode material-theme cyberpunk-theme emmet-mode web-mode markdown-mode haml-mode yaml-mode define-word powerthesaurus flyspell-correct-popup flyspell-correct company shell-pop git-gutter magit counsel-projectile ivy-rich avy flx smex counsel swiper ivy projectile multiple-cursors visual-regexp move-text expand-region which-key vi-tilde-fringe neotree rich-minority smartparens all-the-icons undo-tree simpleclip exec-path-from-shell use-package)))
+    (org-roam org-journal eyebrowse vterm cider paredit clojure-mode material-theme cyberpunk-theme emmet-mode web-mode markdown-mode haml-mode yaml-mode define-word powerthesaurus flyspell-correct-popup flyspell-correct company shell-pop git-gutter magit counsel-projectile ivy-rich avy flx smex counsel swiper ivy projectile multiple-cursors visual-regexp move-text expand-region which-key vi-tilde-fringe neotree rich-minority smartparens all-the-icons undo-tree simpleclip exec-path-from-shell use-package)))
  '(shell-pop-shell-type
    (quote
     ("ansi-term" "*ansi-term*"
